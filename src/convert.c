ae8486ab6fbdacde020fdd83294001ba9abadff4
7klow <202186742+7klow@users.noreply.github.com>
2025-04-22 16:44:32 +0200

    Modification de convert.c

--- src/convert.c
+++ src/convert.c
@@ -1,38 +1,38 @@
-#include <sndfile.h>
-#include <rnnoise.h>
+// _________________________________________          libs               _________________________________________
 
-int get_quality(int quality) {
-  switch (quality) {
-    case 1:
-      return SF_FORMAT_WAV | SF_FORMAT_PCM_16;
-    case 2:
-      return SF_FORMAT_WAV | SF_FORMAT_PCM_24;
-    case 3:
-      return SF_FORMAT_WAV | SF_FORMAT_PCM_32;
-    case 4:
-      return SF_FORMAT_WAV | SF_FORMAT_FLOAT;
-    default:
-      return SF_FORMAT_WAV | SF_FORMAT_PCM_16;
-  }
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// _________________________________________ Get format (mp3, mp4, etc.) _________________________________________
+
+const char* get_format(const char *path) {
+
+  const char *ext = strrchr(path, '.');
+  return ( ext != NULL ) ?  ext + 1 : NULL; // retourne l'extension "mp3", "mp4", etc. (sinon retourne NULL)
 }
 
-int mp3_to_wav(const char *name_in, const char *name_out, int quality) {
-  SF_INFO sfInfo;
-  SNDFILE *file_in = sf_open(name_in, SFM_READ, &sfInfo);
-  if (!file_in)
-    return 1;
-  sfInfo.format = get_quality(quality);
-  SNDFILE *file_out = sf_open(name_out, SFM_WRITE, &sfInfo);
-  if (!file_out) {
-    sf_close(file_in);
-    return 2;
-  }
-  short buf[1024];
-  int c;
-  while ((c = sf_read_short(file_in, buf, 1024)) > 0) {
-    sf_write_short(file_out, buf, c);
-  }
-  sf_close(file_in);
-  sf_close(file_out);
-  return 0;
+// _________________________________________   Convert to wav format     _________________________________________
+
+void convert(const char *path) {
+
+  const char *format  = get_format(path);
+
+  char        cmd[512];
+  char        name[512];
+
+  size_t      lenPath = strlen(path);
+  size_t      lenExt  = strlen(format);
+
+  if (format != NULL && lenPath > lenExt) {
+
+      strncpy(name, path, lenPath - lenExt);
+      name[lenPath - lenExt] = '\0';
+
+  } else { name[0]           = '\0'; }
+
+  if( format != NULL && strcmp(format, "mp4") == 0) { snprintf(cmd, sizeof(cmd), "ffmpeg -y -i \"%s\" -vn -ar 44100 -ac 2 -f wav \"%s.wav\"", path, name); } // Convert un fichier vidéo mp4 en wav
+  else                                              { snprintf(cmd, sizeof(cmd), "ffmpeg -y -i \"%s\" -ar 44100 -ac 2 -f wav \"%s.wav\"",     path, name); } // Convert n'importe ql fichier audio en .wav 
+
+  system(cmd);
 }
