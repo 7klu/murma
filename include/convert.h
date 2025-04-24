87e4ec0592c7afaf8fdb93f2ede0476f8640a3d1
7klow <202186742+7klow@users.noreply.github.com>
2025-04-24 19:02:49 +0200

    Modification de convert.c & convert.h

--- include/convert.h
+++ include/convert.h
@@ -1,6 +1,7 @@
 #ifndef CONVERT_H
 #define CONVERT_H
 
-void convert(const char *path)
+void convert(const char *path);
+const char *get_format(const char *path);
 
 #endif
--- src/convert.c
+++ src/convert.c
@@ -1,35 +1,28 @@
-// _________________________________________          libs               _________________________________________
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// _________________________________________ Get format (mp3, mp4, etc.) _________________________________________
 
 const char* get_format(const char *path) {
 
   const char *ext = strrchr(path, '.');
-  return ( ext != NULL ) ?  ext + 1 : NULL; // retourne l'extension "mp3", "mp4", etc. (sinon retourne NULL)
+  return ext != NULL ?  ext + 1 : NULL;
 }
-
-// _________________________________________   Convert to wav format     _________________________________________
 
 void convert(const char *path) {
 
   const char *format  = get_format(path);
 
-  char        cmd[512];
-  char        name[512];
+  char        cmd[512], name[512];
 
-  size_t      lenPath = strlen(path);
-  size_t      lenExt  = strlen(format);
+  size_t      lenPath = strlen(path), lenExt  = strlen(format);
 
   if (format != NULL && lenPath > lenExt) {
 
-      strncpy(name, path, lenPath - lenExt);
+      strncpy(name, path,  lenPath - lenExt);
       name[lenPath - lenExt] = '\0';
 
-  } else { name[0]           = '\0'; }
+  } else { name[0] = '\0'; }
 
   if( format != NULL && strcmp(format, "mp4") == 0) { snprintf(cmd, sizeof(cmd), "ffmpeg -y -i \"%s\" -vn -ar 44100 -ac 2 -f wav \"%s.wav\"", path, name); } // Convert un fichier vidéo mp4 en wav
   else                                              { snprintf(cmd, sizeof(cmd), "ffmpeg -y -i \"%s\" -ar 44100 -ac 2 -f wav \"%s.wav\"",     path, name); } // Convert n'importe ql fichier audio en .wav 
